05804d78dccad0911ae8b84d5c5fccb4505a520f
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-03 11:22:17 -0700

    Remove line of code for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'auth/google_oauth2/callback', to: 'sessions#auth'
+  get 'auth/google_oauth2/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  root '/', to: 'welcome#index'
 end
