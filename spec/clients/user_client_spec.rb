7b5f9d22ca89e42c9529ab02db171dde20e3b3b9
Laura Court <laura.mcourt@gmail.com>
2021-11-04 01:29:13 -0400

    Add test for fetch method

--- spec/clients/user_client_spec.rb
+++ spec/clients/user_client_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UserClient, :vcr do
-  it 'returns parsed json body' do
+  it 'returns post json data' do
     result = UserClient.post_data('/api/v1/users', { 'email': 'mikeosmonson@gmail.com' })
+    result = result[:data]
+    expect(result.size).to eq(3)
+    expect(result).to be_a(Hash)
+  end
+
+  it 'returns get json data' do
+    result = UserClient.fetch("/api/v1/users/4")
     result = result[:data]
     expect(result.size).to eq(3)
     expect(result).to be_a(Hash)
