1384e355c2911ff014bfe5f580916864d34a23df
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-04 11:41:12 -0600

    Add current_user
    Co-Authored-By: Laura Court <Lmcourt@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :require_user
+
+  private
+
+  def current_user
+    @current_user ||= UserFacade.user_get(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    permission_denied unless current_user
+  end
+
+  def permission_denied
+    redirect_to root_path, alert: 'Please login or create an account.'
+  end
 end
