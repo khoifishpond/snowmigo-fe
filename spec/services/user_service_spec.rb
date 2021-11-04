9d1a8b210ec8f652cbf8b4c02362150525018d60
Laura Court <laura.mcourt@gmail.com>
2021-11-03 23:23:58 -0400

    Update tests
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>
    Co-Authored-By: Khoi Nguyen <khoifishpond@users.noreply.github.com>

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe UserService, :vcr do
     result = UserService.user_auth('mikeosmonson@gmail.com')
 
     expect(result).to be_a(Hash)
-    expect(result.size).to eq(1)
+    expect(result.size).to eq(3)
     expect(result.first).to be_a(Array)
-    expect(result[:data][:attributes][:email]).to eq('mikeosmonson@gmail.com')
+    expect(result[:attributes][:email]).to eq('mikeosmonson@gmail.com')
   end
 end
