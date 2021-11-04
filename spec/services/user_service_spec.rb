0de87495b2d440faebebbae6479bd5b862411e92
Laura Court <laura.mcourt@gmail.com>
2021-11-03 22:59:09 -0400

    Add test for user_auth
    Co-Authored-By: Khoi Nguyen <khoifishpond@users.noreply.github.com>
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe UserService do
+RSpec.describe UserService, :vcr do
   it 'has a users' do
-    result = UserService.user_auth
+    result = UserService.user_auth('mikeosmonson@gmail.com')
 
-    expect(result).to be_a(Array)
+    expect(result).to be_a(Hash)
     expect(result.size).to eq(1)
-    expect(result.first).to be_a(Hash)
-    expect(result.first[:email]).to be_a(String)
+    expect(result.first).to be_a(Array)
+    expect(result[:data][:attributes][:email]).to eq('mikeosmonson@gmail.com')
   end
 end
