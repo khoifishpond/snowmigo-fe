5526c6609f0e9cf9a47b0a6e4be26d6353d89d6f
Laura Court <laura.mcourt@gmail.com>
2021-11-03 23:23:14 -0400

    Add [:data] to user_auth method
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>
    Co-Authored-By: Khoi Nguyen <khoifishpond@users.noreply.github.com>

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,7 +1,7 @@
 class UserService
   class << self
     def user_auth(email)
-      UserClient.post_data('/api/v1/users', { 'email': email })
+      UserClient.post_data('/api/v1/users', { 'email': email })[:data]
     end
   end
 end
