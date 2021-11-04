f4ef29848c96dca06e8287c74e61db09b41915ed
Laura Court <laura.mcourt@gmail.com>
2021-11-04 01:32:49 -0400

    Add name to user_auth/add user_get method
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>
    Co-Authored-By: Khoi Nguyen <khoifishpond@users.noreply.github.com>

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,8 +1,13 @@
 class UserFacade
   class << self
 
-    def user_auth(email)
-      user = UserService.user_auth(email)
+    def user_auth(email, name)
+      user = UserService.user_auth(email, name)
+      UserPoro.new(user)
+    end
+
+    def user_get(id)
+      user = UserService.user_get(id)
       UserPoro.new(user)
     end
   end
