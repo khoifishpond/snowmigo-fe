64771f66ead2e17ef3ed5e547765a41158fff278
Laura Court <laura.mcourt@gmail.com>
2021-11-04 23:13:29 -0400

    Add test for user_update

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -16,4 +16,27 @@ RSpec.describe UserService, :vcr do
     expect(result.size).to eq(3)
     expect(result[:id]).to eq("4")
   end
+
+  it 'updates user info' do
+    result = UserService.user_update(4, {
+                                          name: 'Ozzie',
+                                          email: 'mikeosmonson@gmail.com',
+                                          address: 'Denver',
+                                          ski_pass: 'Epic',
+                                          ski_or_board: 'snow board',
+                                          exp_level: 'expert',
+                                          emergency_name: 'Christina',
+                                          emergency_number: '3945638198'
+                                         })
+    expect(result).to be_a(Hash)
+    expect(result.size).to eq(3)
+    expect(result[:attributes]).to have_key :name
+    expect(result[:attributes]).to have_key :email
+    expect(result[:attributes]).to have_key :address
+    expect(result[:attributes]).to have_key :ski_pass
+    expect(result[:attributes]).to have_key :ski_or_board
+    expect(result[:attributes]).to have_key :exp_level
+    expect(result[:attributes]).to have_key :emergency_name
+    expect(result[:attributes]).to have_key :emergency_number
+  end
 end
