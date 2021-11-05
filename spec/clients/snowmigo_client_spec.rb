856fffc4278f1cd75d0ddbec8b57deede87e2054
Laura Court <laura.mcourt@gmail.com>
2021-11-04 23:12:30 -0400

    Add test for patch data

--- spec/clients/snowmigo_client_spec.rb
+++ spec/clients/snowmigo_client_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe SnowmigoClient, :vcr do
     expect(result.size).to eq(3)
     expect(result).to be_a(Hash)
   end
+
+  it 'returns patch json data' do
+    result = SnowmigoClient.patch_data('/api/v1/users/4', {
+                                                            name: 'Ozzie',
+                                                            email: 'mikeosmonson@gmail.com',
+                                                            address: 'Denver',
+                                                            pass_type: 'Epic',
+                                                            ride_type: 'snow board',
+                                                            xp_level: 'expert',
+                                                            emergency_name: 'Christina',
+                                                            emergency_number: '3945638198'
+                                                           }
+                                                          )
+    result = result[:data]
+    expect(result.size).to eq(3)
+    expect(result).to be_a(Hash)
+  end
 end
