ab7d78d3be2029c5466b60d67d51ddff4f138de8
Laura Court <laura.mcourt@gmail.com>
2021-11-04 23:09:03 -0400

    Add attributes
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>

--- app/poros/user_poro.rb
+++ app/poros/user_poro.rb
@@ -1,9 +1,15 @@
 class UserPoro
-  attr_reader :email, :name, :id
+  attr_reader :email, :name, :id, :address, :ski_pass, :ski_or_board, :exp_level, :emergency_name, :emergency_number
 
   def initialize(data)
     @id = data[:id]
     @name = data[:attributes][:name]
     @email = data[:attributes][:email]
+    @address = data[:attributes][:address]
+    @ski_pass = data[:attributes][:ski_pass]
+    @ski_or_board = data[:attributes][:ski_or_board]
+    @exp_level = data[:attributes][:exp_level]
+    @emergency_name = data[:attributes][:emergency_name]
+    @emergency_number = data[:attributes][:emergency_number]
   end
 end
