66d015df03fb6a89fcea99dc76bec0d143502c9f
Laura Court <laura.mcourt@gmail.com>
2021-11-04 23:08:34 -0400

    Add edit/update action
    Co-Authored-By: Erika Kischuk <eakischuk@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
 
   def show
-# require "pry"; binding.pry
     @user = UserFacade.user_get(params[:id])
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    UserFacade.user_update(current_user.id, updated_attributes)
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def updated_attributes
+   {
+    name: params[:name],
+    address: params[:address],
+    pass_type: params[:pass_type],
+    ski_or_board: params[:ski_or_board],
+    xp_level: params[:xp_level],
+    emergency_name: params[:emergency_name],
+    emergency_number: params[:emergency_number]
+    }
   end
 end
