1bed746191efd93b5d739e3ba0b8a0d972544543
Laura Court <laura.mcourt@gmail.com>
2021-11-06 00:32:31 -0400

    Add logout, trips, and resorts paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#show'
-  get 'auth/google_oauth2/callback', to: 'sessions#create'
+  get 'auth/google_oauth2/callback', controller: :sessions, action: :create
   get 'auth/failure', to: redirect('/')
+  delete '/logout', controller: :sessions, action: :destroy
   resources :users, only: %i[show edit update]
+  resources :trips
+  resources :resorts
 end
