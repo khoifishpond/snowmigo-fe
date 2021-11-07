5573f7cc00db4fecc0629998f550adc0c0b99bf3
Laura Court <laura.mcourt@gmail.com>
2021-11-06 21:39:06 -0400

    Add attributes

---
+++ app/poros/trip_poro.rb
@@ -0,0 +1,13 @@
+class TripPoro
+  attr_reader :id, :start_date, :end_date, :resort_id, :name, :resort_name, :riders
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:attributes][:name]
+    @start_date = data[:attributes][:start_date]
+    @end_date = data[:attributes][:end_date]
+    @riders = data[:relationships][:riders]
+    @resort_id = data[:attributes][:resort_id]
+    @resort_name = data[:attributes][:resort_name]
+  end
+end
