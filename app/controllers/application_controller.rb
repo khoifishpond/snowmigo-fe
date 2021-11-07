d9ebefc65fd874768a72fedd7868c646b4810ccb
Laura Court <laura.mcourt@gmail.com>
2021-11-06 21:38:34 -0400

    Add potential all_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :all_users
   # before_action :require_user
 
   private
 
   def current_user
     @current_user ||= UserFacade.user_get(session[:user_id]) if session[:user_id]
+  end
+
+  def all_users
+    @all_users ||= UserFacade.all_users
   end
 
   # def require_user
