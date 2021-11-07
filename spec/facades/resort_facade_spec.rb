68af2bdef8fd97255f77d85a6a60cfe70d71d630
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-07 12:30:38 -0700

    Add testing for resorts by state API response

--- spec/facades/resort_facade_spec.rb
+++ spec/facades/resort_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ResortFacade do
-  it '#resorts_by_state' do
+  it '#resorts_by_state', :vcr do
     resorts = ResortFacade.resorts_by_state('co')
     expect(resorts).to be_an(Array)
     expect(resorts.first).to be_a(ResortPoro)
--- spec/services/resort_service_spec.rb
+++ spec/services/resort_service_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ResortService do
-  it '#resorts_by_state' do
+  it '#resorts_by_state', :vcr do
     resorts = ResortService.resorts_by_state('co')
     expect(resorts).to be_a Hash
     expect(resorts[:data]).to be_an Array
     resorts[:data].each do |resort|
-      expect(resort[:type]).to eq 'state_resort'
-      expect(resort[:id]).to be_a String
-      expect(resort[:attributes][:name]).to be_a String
+      expect(resort).to be_a(Hash)
+      expect(resort).to have_key(:type)
+      expect(resort[:type]).to be_a(String)
+
+      expect(resort).to have_key(:id)
+      expect(resort[:id]).to be_a(String)
+
+      expect(resort).to have_key(:attributes)
+      expect(resort[:attributes]).to be_a(Hash)
+      expect(resort[:attributes]).to have_key(:name)
+      expect(resort[:attributes][:name]).to be_a(String)
     end
   end
 end
