08dc029df00f900a65bd0a387a6091e68e7b2076
Laura Court <laura.mcourt@gmail.com>
2021-11-06 21:49:47 -0400

    Add get trip test

--- spec/facades/trip_facade_spec.rb
+++ spec/facades/trip_facade_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe TripFacade, :vcr do
       result = TripFacade.create_trip({ name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 })
       expect(result).to be_a(TripPoro)
     end
+
+    it 'gets a trip' do
+      result = TripFacade.trip_get(25)
+      expect(result).to be_a(TripPoro)
+    end
   end
 end
