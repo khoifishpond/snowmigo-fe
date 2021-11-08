b0a45fc46bb245f4f3d34c3b429b0f8b243c1fbf
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-07 19:03:16 -0700

    Add resort by id

--- app/services/resort_service.rb
+++ app/services/resort_service.rb
@@ -3,5 +3,9 @@ class ResortService
     def resorts_by_state(state)
       SnowmigoClient.fetch("/api/v1/resorts?states=#{state}")
     end
+
+    def resort_by_id(id)
+      SnowmigoClient.fetch("/api/v1/resorts/#{id}")
+    end
   end
 end
