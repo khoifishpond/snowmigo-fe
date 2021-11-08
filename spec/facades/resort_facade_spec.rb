a1672b803ed35ece1286ea8e6bc84942209470d6
Erika Kischuk <erika.kischuk@gmail.com>
2021-11-07 19:20:27 -0700

    Add test for resort by id

--- spec/facades/resort_facade_spec.rb
+++ spec/facades/resort_facade_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe ResortFacade do
   it '#resorts_by_state', :vcr do
     resorts = ResortFacade.resorts_by_state('co')
     expect(resorts).to be_an(Array)
+    expect(resorts.count).to eq(35)
     expect(resorts.first).to be_a(ResortPoro)
-    #add array.count expectation
+  end
+
+  it '#resort_by_id', :vcr do
+    resort = ResortFacade.resort_by_id('303001')
+    expect(resort).to be_a(ResortPoro)
   end
 end
