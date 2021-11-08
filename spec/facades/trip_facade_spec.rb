2592af3af5fc927af6a21f4db5c9179807c63fc7
Laura Court <laura.mcourt@gmail.com>
2021-11-07 23:22:48 -0500

    Test update trip

--- spec/facades/trip_facade_spec.rb
+++ spec/facades/trip_facade_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe TripFacade, :vcr do
       result = TripFacade.trip_get(25)
       expect(result).to be_a(TripPoro)
     end
+
+    it 'updates a trip' do
+      result = TripFacade.update_trip(25, { name: 'Happy time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 3 })
+      expect(result).to be_a(TripPoro)
+    end
   end
 end
