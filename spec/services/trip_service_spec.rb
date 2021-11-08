e50859b0ec0c666a60c0373accc7857a23cac399
Laura Court <laura.mcourt@gmail.com>
2021-11-07 23:10:50 -0500

    Update expect size to be 4

--- spec/services/trip_service_spec.rb
+++ spec/services/trip_service_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 #
 RSpec.describe TripService, :vcr do
   it 'posts a trip' do
-    result = TripService.create_trip({ name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 })
     # require "pry"; binding.pry
+    result = TripService.create_trip({ name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 })
     expect(result).to be_a(Hash)
-    expect(result.size).to eq(3)
+    expect(result.size).to eq(4)
     expect(result.first).to be_a(Array)
     expect(result[:attributes][:resort_id]).to eq(2)
   end
@@ -13,7 +13,7 @@ RSpec.describe TripService, :vcr do
   it 'returns a trip' do
     result = TripService.trip_get(47)
     expect(result).to be_a(Hash)
-    expect(result.size).to eq(3)
+    expect(result.size).to eq(4)
     expect(result.first).to be_a(Array)
     expect(result[:attributes][:start_date]).to eq('2021-11-08')
     expect(result[:attributes][:end_date]).to eq('2021-11-25')
@@ -26,10 +26,14 @@ RSpec.describe TripService, :vcr do
                                             end_date: '2021-11-10'
                                             })
     expect(result).to be_a(Hash)
-    expect(result.size).to eq(3)
+    expect(result.size).to eq(4)
     expect(result.first).to be_a(Array)
     expect(result[:attributes]).to have_key :name
     expect(result[:attributes]).to have_key :start_date
     expect(result[:attributes]).to have_key :end_date
   end
+
+  # it 'deletes a trip record' do
+  #   result = TripService.destroy_data
+  # end
 end
