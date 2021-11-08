6f72f26cd3f8d6dae0a06874d08c7ad142dcd7b1
Laura Court <laura.mcourt@gmail.com>
2021-11-08 12:48:57 -0500

    Adjust dates

--- spec/features/trip/show_spec.rb
+++ spec/features/trip/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'trip show page', :vcr do
   describe 'has trip info' do
-    let(:trip) { TripPoro.new({ id: 47, attributes: { name: 'Happy fun time', start_date: '11/08/2021', end_date: '11/25/2021', resort_id: 2 } }) }
+    let(:trip) { TripPoro.new({ id: 47, attributes: { name: 'Happy fun time', start_date: '08/11/2021', end_date: '25/11/2021', resort_id: 2 } }) }
     it 'shows trip info' do
       visit trip_path(trip.id)
       expect(page).to have_content('Nov 8, 2021')
