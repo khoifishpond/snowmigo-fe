9621bf75586aaee984e3cb34ece82ab3a98030de
Laura Court <laura.mcourt@gmail.com>
2021-11-07 20:41:33 -0500

    Test for updating trip

--- spec/services/trip_service_spec.rb
+++ spec/services/trip_service_spec.rb
@@ -18,4 +18,18 @@ RSpec.describe TripService, :vcr do
     expect(result[:attributes][:start_date]).to eq('2021-11-08')
     expect(result[:attributes][:end_date]).to eq('2021-11-25')
   end
+
+  it 'updates a trip' do
+    result = TripService.update_trip(48, {
+                                            name: 'This is a trip',
+                                            start_date: '2021-11-09',
+                                            end_date: '2021-11-10'
+                                            })
+    expect(result).to be_a(Hash)
+    expect(result.size).to eq(3)
+    expect(result.first).to be_a(Array)
+    expect(result[:attributes]).to have_key :name
+    expect(result[:attributes]).to have_key :start_date
+    expect(result[:attributes]).to have_key :end_date
+  end
 end
