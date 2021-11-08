61dc17a3e3a71c24f792fec22ca41c0433eb83e5
Laura Court <laura.mcourt@gmail.com>
2021-11-08 12:48:48 -0500

    Adjust dates and id

--- spec/features/trip/new_spec.rb
+++ spec/features/trip/new_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'New trip', :vcr do
   describe 'creating a trip' do
-    let(:trip) { TripPoro.new({ id: 94, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 } }) }
+    let(:trip) { TripPoro.new({ id: 101, attributes: { name: 'Happy fun time', start_date: '12/12/2021', end_date: '14/12/2021', resort_id: 2 } }) }
     it 'creates a trip' do
       visit new_trip_path
       fill_in :start_date, with: '12/12/2021'
-      fill_in :end_date, with: '12/14/2021'
+      fill_in :end_date, with: '14/12/2021'
       fill_in :name, with: "Happy fun time"
       click_on 'Create trip!'
 
