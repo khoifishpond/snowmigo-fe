f1fd37f7e0a524fbf1e181bfb1236c2b34e3561b
Laura Court <laura.mcourt@gmail.com>
2021-11-07 23:36:22 -0500

    Add tests for date methods

--- spec/poros/trip_poro_spec.rb
+++ spec/poros/trip_poro_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TripPoro do
-  let(:trip) { TripPoro.new({ id: 1, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 } }) }
+  let(:trip) { TripPoro.new({ id: 1, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '11/22/2022', resort_id: 2 } }) }
   it 'has attributes' do
     expect(trip.id).to eq(1)
     expect(trip.name).to eq('Happy fun time')
     expect(trip.start_date).to eq('11/11/1111')
-    expect(trip.end_date).to eq('22/22/2222')
+    expect(trip.end_date).to eq('11/22/2022')
     expect(trip.resort_id).to eq(2)
+  end
+
+  it 'formats the start date' do
+    expect(trip.format_start_date).to eq('Nov 11, 1111')
+  end
+
+  it 'formats the end date' do
+    expect(trip.format_end_date).to eq('Nov 22, 2022')
   end
 end
