0cafcfe330d22b106e2d077012035ffce4b3929c
Laura Court <laura.mcourt@gmail.com>
2021-11-07 23:35:47 -0500

    Update test id

--- spec/features/trip/new_spec.rb
+++ spec/features/trip/new_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'New trip', :vcr do
   describe 'creating a trip' do
-    let(:trip) { TripPoro.new({ id: 25, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 } }) }
+    let(:trip) { TripPoro.new({ id: 94, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '22/22/2222', resort_id: 2 } }) }
     it 'creates a trip' do
       visit new_trip_path
       fill_in :start_date, with: '12/12/2021'
