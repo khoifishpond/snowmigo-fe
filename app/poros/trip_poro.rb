dbf9b11070be3a15a907692a35f6178783dfaa7e
Laura Court <laura.mcourt@gmail.com>
2021-11-07 23:35:36 -0500

    add methods to format dates

--- app/poros/trip_poro.rb
+++ app/poros/trip_poro.rb
@@ -4,10 +4,18 @@ class TripPoro
   def initialize(data)
     @id = data[:id]
     @name = data[:attributes][:name]
-    @start_date = data[:attributes][:start_date].to_date.strftime('%b %-d, %Y')
-    @end_date = data[:attributes][:end_date].to_date.strftime('%b %-d, %Y')
+    @start_date = data[:attributes][:start_date]
+    @end_date = data[:attributes][:end_date]
     # @riders = data[:relationships][:riders]
     @resort_id = data[:attributes][:resort_id]
     @resort_name = data[:attributes][:resort_name]
+  end
+
+  def format_start_date
+    @start_date.to_date.strftime('%b %-d, %Y')
+  end
+
+  def format_end_date
+    @end_date.to_date.strftime('%b %-d, %Y')
   end
 end
