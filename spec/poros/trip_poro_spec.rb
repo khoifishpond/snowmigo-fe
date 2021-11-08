bbd2ba60e86df18a07eb588062d371ec86babc73
Laura Court <laura.mcourt@gmail.com>
2021-11-08 12:49:07 -0500

    Adjust dates

--- spec/poros/trip_poro_spec.rb
+++ spec/poros/trip_poro_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TripPoro do
-  let(:trip) { TripPoro.new({ id: 1, attributes: { name: 'Happy fun time', start_date: '11/11/1111', end_date: '11/22/2022', resort_id: 2 } }) }
+  let(:trip) { TripPoro.new({ id: 47, attributes: { name: 'trip', start_date: '08/11/2021', end_date: '25/11/2021', resort_id: 2 } }) }
   it 'has attributes' do
-    expect(trip.id).to eq(1)
-    expect(trip.name).to eq('Happy fun time')
-    expect(trip.start_date).to eq('11/11/1111')
-    expect(trip.end_date).to eq('11/22/2022')
+    expect(trip.id).to eq(47)
+    expect(trip.name).to eq('trip')
+    expect(trip.start_date).to eq('08/11/2021')
+    expect(trip.end_date).to eq('25/11/2021')
     expect(trip.resort_id).to eq(2)
   end
 
   it 'formats the start date' do
-    expect(trip.format_start_date).to eq('Nov 11, 1111')
+    expect(trip.format_start_date).to eq('Nov 8, 2021')
   end
 
   it 'formats the end date' do
-    expect(trip.format_end_date).to eq('Nov 22, 2022')
+    expect(trip.format_end_date).to eq('Nov 25, 2021')
   end
 end
