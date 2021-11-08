c315607376334bf8d6f4e8de54edf4fe3bc9378c
Laura Court <laura.mcourt@gmail.com>
2021-11-07 20:40:58 -0500

    Add update and destroy

--- app/services/trip_service.rb
+++ app/services/trip_service.rb
@@ -8,5 +8,13 @@ class TripService
     def trip_get(id)
       SnowmigoClient.fetch("/api/v1/trips/#{id}")[:data]
     end
+
+    def update_trip(id, updated_attributes)
+      SnowmigoClient.patch_data("/api/v1/trips/#{id}", updated_attributes)[:data]
+    end
+
+    def destroy_trip(id)
+      SnowmigoClient.destroy_data("/api/v1/trips/#{id}")
+    end
   end
 end
