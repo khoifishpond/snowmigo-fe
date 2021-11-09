b93740a4e139f83a2ff4318d295a9b3f5eadd431
Henry Schmid <henry.schmid1@gmail.com>
2021-11-08 21:54:06 -0700

    Refactor trip update test

--- spec/features/trip/edit_spec.rb
+++ spec/features/trip/edit_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 describe 'edit page', :vcr do
+  it 'can update a trip' do
+    trip = TripFacade.trip_get(113)
+    visit edit_trip_path(trip.id)
+    
+    expect(page).to have_field(:name, with: "#{trip.name}")
+    expect(page).to have_field(:start_date, with: "#{trip.start_date}")
+    expect(page).to have_field(:end_date, with: "#{trip.end_date}")
 
-  before :each do
-    @trip = TripFacade.trip_get(47)
-    visit edit_trip_path(@trip.id)
-  end
+    fill_in :name, with: 'Cool Trip Name'
+    click_button 'Edit trip!'
 
-  it 'shows trip on edit page' do
-    expect(page).to have_field(:name, with: "#{@trip.name}")
-    expect(page).to have_field(:start_date, with: "#{@trip.start_date}")
-    expect(page).to have_field(:end_date, with: "#{@trip.end_date}")
+    expect(current_path).to eq(trip_path(trip.id))
+    expect(page).to have_content('Cool Trip Name')
   end
 end
