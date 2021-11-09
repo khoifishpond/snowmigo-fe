a2b821c78fce5907de20503cd05879ed4815c39b
Henry Schmid <henry.schmid1@gmail.com>
2021-11-08 20:00:32 -0700

    Merge PR from Ozzie

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'welcome page' do
-  before(:each) do
-    visit root_path
-  end
-  it 'has welcome page' do
-    expect(page).to have_content("Welcome to Snowmigo!")
-    expect(page).to have_button("Login with Google")
-  end
+RSpec.describe 'Welcome page' do
+  describe 'As a visitor' do
+    before { visit root_path }
 
-  xit 'login redirects to user dashboard', :vcr do
-    click_on 'Login with Google'
-    expect(page).to have_content("Welcome Freddie Mercury")
+    it 'displays Welcome text and button' do
+      expect(page).to have_content('Welcome to Snowmigo!')
+      expect(page).to have_button('Login with Google')
+    end
+
+    it 'login redirects to user dashboard', :vcr do
+      click_on 'Login with Google'
+      expect(page).to have_content('Welcome Freddie Mercury')
+    end
   end
 end
