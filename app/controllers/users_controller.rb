58e768a335b8577bdf2b64ba55ae25f0c1b90b62
Henry Schmid <henry.schmid1@gmail.com>
2021-11-08 19:44:16 -0700

    Refactor user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,21 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = UserFacade.user_update(current_user.id, updated_attributes)
+    UserFacade.user_update(current_user.id, updated_attributes)
     redirect_to user_path(current_user.id)
   end
 
   private
 
   def updated_attributes
-   {
-    name: params[:name],
-    address: params[:address],
-    ski_pass: params[:ski_pass],
-    ski_or_board: params[:ski_or_board],
-    exp_level: params[:exp_level],
-    emergency_name: params[:emergency_name],
-    emergency_number: params[:emergency_number]
+    {
+      name: params[:name],
+      address: params[:address],
+      ski_pass: params[:ski_pass],
+      ski_or_board: params[:ski_or_board],
+      exp_level: params[:exp_level],
+      emergency_name: params[:emergency_name],
+      emergency_number: params[:emergency_number]
     }
   end
 end
