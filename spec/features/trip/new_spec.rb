ad5589804e687e0d6b739b5e39c6112ddb159cb3
Henry Schmid <henry.schmid1@gmail.com>
2021-11-10 15:49:49 -0700

    Update tests for new ids

--- spec/features/trip/new_spec.rb
+++ spec/features/trip/new_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'New trip', :vcr do
   describe 'creating a trip' do
     let(:trip) do
-      Trip.new({ data: {
-                 id: 155,
+      Trip.new({ id: 315,
                  attributes: { name: 'Happy fun time',
                                start_date: '12/12/2021',
                                end_date: '14/12/2021',
-                               resort_id: 2 }
-               } })
+                               resort_id: 2,
+                               user_id: 19 } })
     end
-    
+
     it 'creates a trip' do
+      login_with_oauth
       visit new_trip_path
       fill_in :start_date, with: '12/12/2021'
       fill_in :end_date, with: '14/12/2021'
