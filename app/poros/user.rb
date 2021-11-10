7005bfbbd3fa9114202120aae890682764b23768
Ozzie <mikeosmonson@gmail.com>
2021-11-10 11:28:02 -0700

    Add #past_trips #upcoming_trips #get_friend to create objects and separate the data sets

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_reader :email, :name, :id, :address, :ski_pass, :ski_or_board, :exp_level, :emergency_name, :emergency_number
+  attr_reader :email, :name, :id, :address, :ski_pass, :ski_or_board, :exp_level, :emergency_name, :emergency_number, :trips, :friends
 
   def initialize(data)
     @id = data[:id]
@@ -11,5 +11,25 @@ class User
     @exp_level = data[:attributes][:exp_level]
     @emergency_name = data[:attributes][:emergency_name]
     @emergency_number = data[:attributes][:emergency_number]
+    @trips = data[:attributes][:trips]
+    @friends = data[:attributes][:friends]
+  end
+
+  def past_trips
+    @trips.map do |trip|
+      Trip.new(trip) if trip.end_date < Date.today
+    end
+  end
+
+  def upcoming_trips
+    @trip.map do |trip|
+      Trip.new(trip) if trip.end_date >= Date.today
+    end
+  end
+
+  def get_friend
+    @friends.map do |friend|
+      Friend.new(friend)
+    end
   end
 end
