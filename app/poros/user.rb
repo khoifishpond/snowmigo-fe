cad9dbefff6f6c6a3e0170e4ac840e5254000a90
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-09 18:24:14 -0800

    Update User poro and service

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,15 +1,22 @@
 class User
   attr_reader :email, :name, :id, :address, :ski_pass, :ski_or_board, :exp_level, :emergency_name, :emergency_number
 
-  def initialize(data)
-    @id = data[:id]
-    @name = data[:attributes][:name]
-    @email = data[:attributes][:email]
-    @address = data[:attributes][:address]
-    @ski_pass = data[:attributes][:ski_pass]
-    @ski_or_board = data[:attributes][:ski_or_board]
-    @exp_level = data[:attributes][:exp_level]
-    @emergency_name = data[:attributes][:emergency_name]
-    @emergency_number = data[:attributes][:emergency_number]
+  def initialize(info[:data])
+    @id = info[:data][:id]
+    @name = info[:data][:attributes][:name]
+    @email = info[:data][:attributes][:email]
+    @address = info[:data][:attributes][:address]
+    @ski_pass = info[:data][:attributes][:ski_pass]
+    @ski_or_board = info[:data][:attributes][:ski_or_board]
+    @exp_level = info[:data][:attributes][:exp_level]
+    @emergency_name = info[:data][:attributes][:emergency_name]
+    @emergency_number = info[:data][:attributes][:emergency_number]
+    @friend_info = info[:included]
+  end
+
+  def friends
+    @friend_info.map do |friend|
+      friend[:attributes][:name]
+    end
   end
 end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,15 +2,15 @@ class UserService
   class << self
     def user_auth(email, name)
       SnowmigoClient.post_data('/api/v1/users',
-                               { 'email': email, 'name': name })[:data]
+                               { 'email': email, 'name': name })
     end
 
     def user_get(id)
-      SnowmigoClient.fetch("/api/v1/users/#{id}")[:data]
+      SnowmigoClient.fetch("/api/v1/users/#{id}")
     end
 
     def user_update(id, updated_attributes)
-      SnowmigoClient.patch_data("/api/v1/users/#{id}", updated_attributes)[:data]
+      SnowmigoClient.patch_data("/api/v1/users/#{id}", updated_attributes)
     end
   end
 end
