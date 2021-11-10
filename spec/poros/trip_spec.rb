379bdc6b62e4faea4024eff147b226d4db1e7dde
Laura Court <laura.mcourt@gmail.com>
2021-11-10 18:31:18 -0500

    Test trip attributes

--- spec/poros/trip_spec.rb
+++ spec/poros/trip_spec.rb
@@ -6,14 +6,44 @@ RSpec.describe Trip do
                        attributes: { name: 'trip',
                                      start_date: '08/11/2021',
                                      end_date: '25/11/2021',
-                                     resort_id: 2 } } })
+                                     vote_status: 'open',
+                                     resort_id: 2,
+                                     riders: [{
+                                                data: {
+                                                  attributes: {
+                                                    user_id: 1,
+                                                    trip_id: 47,
+                                                    host: false,
+                                                    driver: false,
+                                                    budget: 42,
+                                                    rider_name: "Khoi"
+                                                  }
+                                                }
+                                      }],
+                                      resort_options: [{
+                                        data: {
+                                          id: 1,
+                                          attributes: {
+                                            trip_id: 47,
+                                            resort_id: 2,
+                                            resort_name: 'Granby',
+                                            vote_count: 0
+                                          }
+                                        }
+                                      }]
+                                   }
+                      }
+              })
   end
   it 'has attributes' do
     expect(trip.id).to eq(47)
     expect(trip.name).to eq('trip')
     expect(trip.start_date).to eq('08/11/2021')
     expect(trip.end_date).to eq('25/11/2021')
+    expect(trip.vote_status).to eq("open")
     expect(trip.resort_id).to eq(2)
+    expect(trip.riders.first[:data][:attributes][:rider_name]).to eq('Khoi')
+    expect(trip.resort_options.first[:data][:attributes][:resort_name]).to eq('Granby')
   end
 
   it 'formats the start date' do
