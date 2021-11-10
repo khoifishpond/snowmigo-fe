5fd509e48a0f5afb9240dc06447b3221f19ee0f1
Henry Schmid <henry.schmid1@gmail.com>
2021-11-10 15:53:09 -0700

    Add :data back to service methods

--- app/services/trip_service.rb
+++ app/services/trip_service.rb
@@ -1,15 +1,15 @@
 class TripService
   class << self
     def create_trip(trip_params)
-      SnowmigoClient.post_data('/api/v1/trips', trip_params)
+      SnowmigoClient.post_data('/api/v1/trips', trip_params)[:data]
     end
 
     def trip_get(id)
-      SnowmigoClient.fetch("/api/v1/trips/#{id}")
+      SnowmigoClient.fetch("/api/v1/trips/#{id}")[:data]
     end
 
     def update_trip(id, updated_attributes)
-      SnowmigoClient.patch_data("/api/v1/trips/#{id}", updated_attributes)
+      SnowmigoClient.patch_data("/api/v1/trips/#{id}", updated_attributes)[:data]
     end
 
     def destroy_trip(id)
@@ -17,7 +17,11 @@ class TripService
     end
 
     def create_resort_option(option_params)
-      SnowmigoClient.post_data("/api/v1/resort_options", option_params)
+      SnowmigoClient.post_data("/api/v1/resort_options", option_params)[:data]
+    end
+
+    def vote(trip_id, vote_params)
+      SnowmigoClient.patch_data("/api/v1/trips/#{trip_id}/vote", vote_params)
     end
   end
 end
