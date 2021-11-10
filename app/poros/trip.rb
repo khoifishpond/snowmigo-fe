db354deb978bf7f75ce970d79b704d7830b8c3d0
Laura Court <laura.mcourt@gmail.com>
2021-11-10 18:30:06 -0500

    Update attributes

--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -1,16 +1,15 @@
 class Trip
-  attr_reader :id, :start_date, :end_date, :resort_id, :name, :resort_name,
-              :options
+  attr_reader :id, :start_date, :end_date, :resort_id, :name, :resort_options, :vote_status, :riders
 
   def initialize(info)
     @id = info[:data][:id]
     @name = info[:data][:attributes][:name]
     @start_date = info[:data][:attributes][:start_date]
     @end_date = info[:data][:attributes][:end_date]
-    # @riders = info[:data][:relationships][:riders]
+    @vote_status = info[:data][:attributes][:vote_status]
     @resort_id = info[:data][:attributes][:resort_id]
-    @resort_name = info[:data][:attributes][:resort_name]
-    @option_info = info[:included]
+    @riders = info[:data][:attributes][:riders]
+    @resort_options = info[:data][:attributes][:resort_options]
   end
 
   def format_start_date
@@ -19,11 +18,5 @@ class Trip
 
   def format_end_date
     @end_date.to_date.strftime('%b %-d, %Y')
-  end
-
-  def options
-    @option_info.map do |option|
-      option[:attributes]
-    end
   end
 end
