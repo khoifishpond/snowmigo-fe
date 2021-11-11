44709bd0a84ae82dee12fad8d1f97e1f1654dda2
Laura Court <laura.mcourt@gmail.com>
2021-11-11 00:20:33 -0500

    Remove [:data] from attribute hash
    Co-Authored-By: Henry Schmid <hschmid516@users.noreply.github.com>
    Co-Authored-By: Khoi Nguyen <khoifishpond@users.noreply.github.com>

--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -1,15 +1,15 @@
 class Trip
-  attr_reader :id, :start_date, :end_date, :resort_id, :name, :resort_options, :vote_status, :riders
+  attr_reader :id, :start_date, :end_date, :resort_id, :name, :options, :vote_status, :riders
 
   def initialize(info)
-    @id = info[:data][:id]
-    @name = info[:data][:attributes][:name]
-    @start_date = info[:data][:attributes][:start_date]
-    @end_date = info[:data][:attributes][:end_date]
-    @vote_status = info[:data][:attributes][:vote_status]
-    @resort_id = info[:data][:attributes][:resort_id]
-    @riders = info[:data][:attributes][:riders]
-    @resort_options = info[:data][:attributes][:resort_options]
+    @id = info[:id]
+    @name = info[:attributes][:name]
+    @start_date = info[:attributes][:start_date]
+    @end_date = info[:attributes][:end_date]
+    @vote_status = info[:attributes][:vote_status]
+    @resort_id = info[:attributes][:resort_id]
+    @riders = info[:attributes][:riders]
+    @options = info[:attributes][:resort_options]
   end
 
   def format_start_date
