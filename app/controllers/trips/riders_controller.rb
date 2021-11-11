cabda643ac2ba7d8c690e2e1a5bd81ad0b0fc132
Henry Schmid <henry.schmid1@gmail.com>
2021-11-11 13:56:51 -0700

    Remove pry

--- app/controllers/trips/riders_controller.rb
+++ app/controllers/trips/riders_controller.rb
@@ -3,12 +3,10 @@ class Trips::RidersController < ApplicationController
     rider_params = {
       user_id: params[:user_id],
       trip_id: params[:trip_id],
-      # make host false by default?
       host: params[:host],
       driver: params[:driver],
       budget: params[:budget]
     }
-    require 'pry'; binding.pry
     TripFacade.create_rider(rider_params)
     redirect_to trip_path(params[:trip_id])
   end
