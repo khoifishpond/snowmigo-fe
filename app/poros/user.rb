2cd43ca2e465f92c817e20f5cb09cde517f2b34f
Ozzie <mikeosmonson@gmail.com>
2021-11-10 21:18:39 -0700

    Refactor Methods

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,30 +1,30 @@
 class User
   attr_reader :email, :name, :id, :address, :ski_pass, :ski_or_board, :exp_level, :emergency_name, :emergency_number, :trips, :friends
 
-  def initialize(data)
-    # require "pry"; binding.pry
-    @id = data[:id]
-    @name = data[:attributes][:name]
-    @email = data[:attributes][:email]
-    @address = data[:attributes][:address]
-    @ski_pass = data[:attributes][:ski_pass]
-    @ski_or_board = data[:attributes][:ski_or_board]
-    @exp_level = data[:attributes][:exp_level]
-    @emergency_name = data[:attributes][:emergency_name]
-    @emergency_number = data[:attributes][:emergency_number]
-    @trips = data[:attributes][:trips]
-    @friends = data[:attributes][:friends]
+  def initialize(params)
+    @id = params[:id]
+    @name = params[:attributes][:name]
+    @email = params[:attributes][:email]
+    @address = params[:attributes][:address]
+    @ski_pass = params[:attributes][:ski_pass]
+    @ski_or_board = params[:attributes][:ski_or_board]
+    @exp_level = params[:attributes][:exp_level]
+    @emergency_name = params[:attributes][:emergency_name]
+    @emergency_number = params[:attributes][:emergency_number]
+    @trips = params[:attributes][:trips]
+    @friends = params[:attributes][:friends]
   end
 
   def past_trips
     @trips.filter_map do |trip|
-      trip if trip[:end_date] < Date.today.to_s
+      trip if Date.parse(trip[:data][:attributes][:end_date]).past?
     end
   end
 
   def upcoming_trips
     @trips.filter_map do |trip|
-      trip if trip[:end_date] >= Date.today.to_s
+      # binding.pry
+      trip if Date.parse(trip[:data][:attributes][:end_date]) >= Date.today || !trip[:data][:attributes][:end_date].nil?
     end
   end
 end
