729089b14d831eb51dc856c1ed67a0475a2b96d5
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-11 16:38:42 -0800

    Update have_button to have_link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Welcome page' do
 
     it 'displays Welcome text and button' do
       expect(page).to have_content('Welcome to Snowmigo!')
-      expect(page).to have_button('Login with Google')
+      expect(page).to have_link('Login with Google')
     end
 
     it 'login redirects to user dashboard', :vcr do
